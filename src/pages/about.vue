43efa1ea70750fe98821b8c3a39b563b9453a715
MaxLobinsky <max.lobinsky@gmail.com>
2022-09-12 12:06:21 +0300

    form transfer

---
+++ src/pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="md-body">
+    <h1>В этом приложении:</h1>
+    <ul>
+      <li>-</li>
+      <li>-</li>
+    </ul>
+  </div>
+</template>
---
+++ src/pages/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="md-body">
+    <Form @onSubmit="handleSubmit" />
+  </div>
+</template>
+
+<script>
+import Form from "@/components/Notes/Form.vue";
+export default {
+  components: {
+    Form,
+  },
+  methods: {
+    handleSubmit(note) {
+      console.log(note);
+    },
+  },
+};
+</script>
