84189153764e209a1068114b13ad2e5b60099873
MaxLobinsky <max.lobinsky@gmail.com>
2022-09-13 17:18:47 +0300

    tags added

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="md-body">
-    <Form @onSubmit="handleSubmit" />
-  </div>
+  <Form @onSubmit="handleSubmit" />
+  <List @onRemove="handleRemove" :items="notes" />
 </template>
 
 <script>
 import Form from "@/components/Notes/Form.vue";
+import List from "@/components/Notes/List.vue";
 export default {
   components: {
     Form,
+    List,
+  },
+  data() {
+    return {
+      notes: ["task 1", "task 2", "task 3"],
+    };
   },
   methods: {
     handleSubmit(note) {
-      console.log(note);
+      this.notes.push(note);
+    },
+    handleRemove(index) {
+      this.notes.splice(index, 1);
     },
   },
 };
